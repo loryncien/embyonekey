3df86434c5a9050106eb2fc08f3e98604cc1ea50
loryncien <lluocheng88@gmail.com>
2020-11-02 19:08:25 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd /var/packages/EmbyServer/target/mono/bin
-curl https://raw.githubusercontent.com/w87777w/embyonekey/master/20mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
+wget https://raw.githubusercontent.com/loryncien/embyonekey/master/mb3admin.com.cert.pem
+cat mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
 cd /usr/local/etc/nginx/conf.d/*-*-*-*
-wget https://raw.githubusercontent.com/w87777w/embyonekey/master/user.conf
+wget https://raw.githubusercontent.com/loryncien/embyonekey/master/user.conf
 nginx -s reload
-
